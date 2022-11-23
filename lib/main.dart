489903e1ddc58c7c916d039c90a23ae60e11db72
Apridev <apriantofajar11@gmail.com>
2022-11-23 14:09:01 +0900

    main data

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/pages/onboarding_page.dart';
+import 'package:heartfilia_app/pages/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/' :(context) => SplashScreen(),
+        '/onboarding-page' :(context) => OnboardingPage()
+      },
+    );
+  }
+}
