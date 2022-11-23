87e801fc15773bf884f24130976842e846d3c97c
Apridev <apriantofajar11@gmail.com>
2022-11-23 14:09:11 +0900

    template app

---
+++ lib/template.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = Color(0xff342B38);
+Color secondaryBlackColor = Color(0xff535353);
+Color pinkOne = Color(0xffF89E9E);
+Color pinkTwo = Color(0xffFFAFAF);
+Color pinkThree = Color(0xffFAEFF1);
+Color greenOne = Color(0xff8ADDAE);
+Color greenTwo = Color(0xff64D294);
+Color grayOne = Color(0xffB6B6B6);
+
+TextStyle primaryTextStyle = GoogleFonts.nunito(
+  color: blackColor
+);
+
+TextStyle secondaryBlackStyle = GoogleFonts.nunito(
+  color: secondaryBlackColor
+);
+
+TextStyle pinkOneStyle = GoogleFonts.nunito(
+  color: pinkOne
+);
+
+TextStyle pinkTwoStyle = GoogleFonts.nunito(
+  color: pinkTwo
+);
+
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
