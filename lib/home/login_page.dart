5fbb4f1a88907b4ff84774d1567a7652eba544ec
Apridev <apriantofajar11@gmail.com>
2022-11-29 12:56:16 +0900

    login_page

--- lib/home/login_page.dart
+++ lib/home/login_page.dart
@@ -16,162 +16,272 @@ class LoginPage extends StatelessWidget {
             },
             child: Icon(Icons.close, color: secondaryBlackColor)),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-              child: Image.asset(
-            'assets/images/skincare_login.png',
-            width: MediaQuery.of(context).size.height * 0.12,
-          )),
-          Container(
-            margin:
-                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
-            child: Center(
-              child: Text(
-                'Do login',
-                style: primaryTextStyle.copyWith(
-                    fontSize: 16, fontWeight: semiBold),
-              ),
-            ),
-          ),
-          Container(
-            margin:
-                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.01),
-            child: Center(
-              child: Text(
-                'Start logging in before you start your \nself-care',
-                style:
-                    primaryTextStyle.copyWith(fontSize: 14, fontWeight: medium),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.04,
-                  left: 20,
-                  right: 20),
-              child: TextField(
-                textAlign: TextAlign.start,
-                textAlignVertical: TextAlignVertical.center,
-                keyboardType: TextInputType.emailAddress,
-                cursorColor: graythree,
-                style: TextStyle(color: blackColor),
-                // obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: pinkOne)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: Colors.black26)),
-                    // prefixIcon: Icon(Icons.person),
-                    prefixIcon: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/icons/icon_username.png',
-                          height: 20,
-                        ),
-                      ],
-                    ),
-                    hintText: "Please enter your username",
-                    hintStyle: secondaryBlackStyle.copyWith(fontSize: 14)),
-              )),
-          Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.03,
-                  left: 20,
-                  right: 20),
-              child: TextField(
-                textAlign: TextAlign.start,
-                textAlignVertical: TextAlignVertical.center,
-                keyboardType: TextInputType.emailAddress,
-                cursorColor: graythree,
-                style: TextStyle(color: blackColor),
-                // obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: pinkOne)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: Colors.black26)),
-                    // prefixIcon: Icon(Icons.person),
-                    prefixIcon: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/icons/icon_phonenumber.png',
-                          height: 20,
-                        ),
-                      ],
-                    ),
-                    hintText: "Please enter your mobile phone",
-                    hintStyle: secondaryBlackStyle.copyWith(fontSize: 14)),
-              )),
-          Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.03,
-                  left: 20,
-                  right: 20),
-              child: TextField(
-                textAlign: TextAlign.start,
-                textAlignVertical: TextAlignVertical.center,
-                keyboardType: TextInputType.emailAddress,
-                cursorColor: graythree,
-                style: TextStyle(color: blackColor),
-                // obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: pinkOne)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: Colors.black26)),
-                    // prefixIcon: Icon(Icons.person),
-                    prefixIcon: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/icons/icon_password.png',
-                          height: 20,
-                        ),
-                      ],
-                    ),
-                    hintText: "6-10 Characters of number or letters",
-                    hintStyle: secondaryBlackStyle.copyWith(fontSize: 14)),
+      body: ListView(
+        children: 
+          [Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                  child: Image.asset(
+                'assets/images/skincare_login.png',
+                width: MediaQuery.of(context).size.height * 0.12,
               )),
               Container(
-                margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.04
-                ),
-                child: Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 20
+                margin:
+                    EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
+                child: Center(
+                  child: Text(
+                    'Do login',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 16, fontWeight: semiBold),
                   ),
+                ),
+              ),
+              Container(
+                margin:
+                    EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.01),
+                child: Center(
+                  child: Text(
+                    'Start logging in before you start your \nself-care',
+                    style:
+                        primaryTextStyle.copyWith(fontSize: 14, fontWeight: medium),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.04,
+                      left: 20,
+                      right: 20),
+                  child: TextField(
+                    textAlign: TextAlign.start,
+                    textAlignVertical: TextAlignVertical.center,
+                    keyboardType: TextInputType.emailAddress,
+                    cursorColor: graythree,
+                    style: TextStyle(color: blackColor),
+                    // obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide(color: pinkOne)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide(color: Colors.black26)),
+                        // prefixIcon: Icon(Icons.person),
+                        prefixIcon: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/icons/icon_username.png',
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                        hintText: "Please enter your username",
+                        hintStyle: secondaryBlackStyle.copyWith(fontSize: 14)),
+                  )),
+              Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.03,
+                      left: 20,
+                      right: 20),
+                  child: TextField(
+                    textAlign: TextAlign.start,
+                    textAlignVertical: TextAlignVertical.center,
+                    keyboardType: TextInputType.emailAddress,
+                    cursorColor: graythree,
+                    style: TextStyle(color: blackColor),
+                    // obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide(color: pinkOne)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide(color: Colors.black26)),
+                        // prefixIcon: Icon(Icons.person),
+                        prefixIcon: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/icons/icon_phonenumber.png',
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                        hintText: "Please enter your mobile phone",
+                        hintStyle: secondaryBlackStyle.copyWith(fontSize: 14)),
+                  )),
+              Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.03,
+                      left: 20,
+                      right: 20),
+                  child: TextField(
+                    textAlign: TextAlign.start,
+                    textAlignVertical: TextAlignVertical.center,
+                    keyboardType: TextInputType.emailAddress,
+                    cursorColor: graythree,
+                    style: TextStyle(color: blackColor),
+                    // obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide(color: pinkOne)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide(color: Colors.black26)),
+                        // prefixIcon: Icon(Icons.person),
+                        prefixIcon: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/icons/icon_password.png',
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                        hintText: "6-10 Characters of number or letters",
+                        hintStyle: secondaryBlackStyle.copyWith(fontSize: 14)),
+                  )),
+              Container(
+                margin:
+                    EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.04),
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
                   width: double.infinity,
                   height: MediaQuery.of(context).size.height * 0.07,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(18),
-                    color: pinkOne
-                  ),
+                      borderRadius: BorderRadius.circular(18), color: pinkOne),
                   child: TextButton(
-                    onPressed: (){},
-                    child: Text('Login', style: backgroundColorStyle.copyWith(
-                      fontSize: 14,
-                      fontWeight: medium
-                    ),),
+                    onPressed: () {},
+                    child: Text(
+                      'Login',
+                      style: backgroundColorStyle.copyWith(
+                          fontSize: 14, fontWeight: medium),
+                    ),
                   ),
                 ),
               ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    top: MediaQuery.of(context).size.height * 0.02),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Image.asset('assets/icons/icon_remember_me_off.png',
+                              width: MediaQuery.of(context).size.width * 0.07),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            'Remember me',
+                            style: primaryTextStyle.copyWith(
+                                fontSize: 14, fontWeight: medium),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Have an account?',
+                          style: primaryTextStyle.copyWith(
+                              fontSize: 14, fontWeight: medium),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          'Login',
+                          style: pinkOneStyle.copyWith(
+                              fontSize: 14, fontWeight: semiBold),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02,
+                      left: 20,
+                      right: 20),
+                  child: Image.asset('assets/images/or_account.png')),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: MediaQuery.of(context).size.height * 0.02
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: grayOne, width: 1)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icons/icon_google.png',
+                            width: 24,
+                          ),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text(
+                            'Google',
+                            style: primaryTextStyle.copyWith(
+                                fontSize: 14, fontWeight: medium),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: grayOne, width: 1)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icons/icon_facebook.png',
+                            width: 22,
+                          ),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text(
+                            'Facebook',
+                            style: primaryTextStyle.copyWith(
+                                fontSize: 14, fontWeight: medium),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
