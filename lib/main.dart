b260d9871e574ed161a43a3514ca4ceb185a40a5
Apridev <apriantofajar11@gmail.com>
2022-12-01 11:26:21 +0900

    main data

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:heartfilia_app/home/register_page.dart';
 import 'package:heartfilia_app/home/login_page.dart';
 import 'package:heartfilia_app/home/reset_password.dart';
+import 'package:heartfilia_app/home/verification-page.dart';
 import 'package:heartfilia_app/pages/onboarding_page.dart';
 import 'package:heartfilia_app/pages/splash_screen.dart';
 
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         '/login-page' :(context) => LoginPage(),
         '/register-page' :(context) => RegisterPage(),
         '/reset-password' :(context) => ResetPasswordPage(),
+        '/verification-page' :(context) => VerificationPage(),
       },
     );
   }
