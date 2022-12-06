8ead9784337425c71dc54ec2bdfd7cb43164e718
Apridev <apriantofajar11@gmail.com>
2022-12-06 21:48:44 +0900

    navigationbar page

--- lib/components/navigationbar-page.dart
+++ lib/components/navigationbar-page.dart
@@ -1,21 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:heartfilia_app/components/calender_page.dart';
+import 'package:heartfilia_app/components/chat_page.dart';
+import 'package:heartfilia_app/components/home_page.dart';
+import 'package:heartfilia_app/components/like_page.dart';
+import 'package:heartfilia_app/home/login_page.dart';
 import 'package:heartfilia_app/template.dart';
 
-class NavigationbarPage extends StatelessWidget {
-  const NavigationbarPage({Key? key}) : super(key: key);
+class NavigationbarPage extends StatefulWidget {
+
+  @override
+  State<NavigationbarPage> createState() => _NavigationbarPageState();
+}
+
+class _NavigationbarPageState extends State<NavigationbarPage> {
+
+    int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    Widget buttomNavbar() {
+      return Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: secondaryBlackColor,
+            )
+          ]
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: backgroundColor,
+          onTap: (value){
+            setState(() {
+              currentIndex = value;
+              print(value);
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding: EdgeInsets.only(
+                    top: 12
+                  ),
+                    child: currentIndex == 0 ? Image.asset(
+                  'assets/icons/icon_home_active.png',
+                  width: MediaQuery.of(context).size.width * 0.07,
+                ) : Image.asset(
+                  'assets/icons/icon_home.png',
+                  width: MediaQuery.of(context).size.width * 0.07,
+                ) ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding: EdgeInsets.only(
+                    top: 12,
+                  ),
+                  child: currentIndex == 1 ? Image.asset(
+                    'assets/icons/icon_calender_active.png',
+                    width: MediaQuery.of(context).size.width * 0.07,
+                  ): Image.asset(
+                    'assets/icons/icon_calender.png',
+                    width: MediaQuery.of(context).size.width * 0.07,
+                  )
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding: EdgeInsets.only(
+                    top: 12
+                  ),
+                  child: currentIndex == 2 ? Image.asset(
+                    'assets/icons/icon_chat_active.png',
+                    width: MediaQuery.of(context).size.width * 0.07,
+                  ) : Image.asset(
+                    'assets/icons/icon_chat.png',
+                    width: MediaQuery.of(context).size.width * 0.07,
+                  ),
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding: EdgeInsets.only(
+                    top: 12
+                  ),
+                  child: currentIndex == 3 ? Image.asset(
+                    'assets/icons/icon_like_active.png',
+                    width: MediaQuery.of(context).size.width * 0.07,
+                  ) : Image.asset(
+                    'assets/icons/icon_like.png',
+                    width: MediaQuery.of(context).size.width * 0.07,
+                  ),
+                ),
+                label: ''),
+          ],
+        ),
+      );
+    }
+
+    Widget bodyHome(){
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+          break;
+        case 1:
+          return CalenderPage();
+          break;
+        case 2:
+          return ChatPage();
+          break;
+        case 3:
+          return LikePage();
+          break;
+        default:
+        return HomePage();
+      }
+    }
+
     return Scaffold(
-      backgroundColor: backgroundColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text('Navigationbar Page')
-          )
-        ],
-      ),
-    );
+        backgroundColor: backgroundColor, 
+        bottomNavigationBar: buttomNavbar(),
+        body: bodyHome() ,
+        );
   }
 }
