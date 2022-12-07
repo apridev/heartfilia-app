77af133234ba296e751cbf93010021c8b7f3b387
Apridev <apriantofajar11@gmail.com>
2022-12-07 22:35:51 +0900

    calender page

---
+++ lib/components/calender_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CalenderPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('Calender Page'))
+        ],
+      ),
+    );
+  }
+}
