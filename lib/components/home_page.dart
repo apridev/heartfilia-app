66fb08d15823f3bd0c9ef6f4cc79e5e1334ca271
Apridev <apriantofajar11@gmail.com>
2022-12-14 12:59:18 +0900

    home page product flash sale

--- lib/components/home_page.dart
+++ lib/components/home_page.dart
@@ -101,16 +101,13 @@ class HomePage extends StatelessWidget {
                 child: Image.asset('assets/images/image_slader.png')),
             Container(
               margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.02
-              ),
+                  top: MediaQuery.of(context).size.height * 0.02),
               child: AnimatedContainer(
                 duration: Duration(milliseconds: 15),
                 width: 12,
                 height: 12,
-                decoration: BoxDecoration(
-                  color: pinkOne,
-                  shape: BoxShape.circle
-                ),
+                decoration:
+                    BoxDecoration(color: pinkOne, shape: BoxShape.circle),
               ),
             )
           ],
@@ -118,91 +115,91 @@ class HomePage extends StatelessWidget {
       );
     }
 
-    Widget categories(){
+    Widget categories() {
       return Container(
         margin: EdgeInsets.only(
-          top: MediaQuery.of(context).size.height * 0.03,
-          left: 20,
-          right: 20
-        ),
+            top: MediaQuery.of(context).size.height * 0.03,
+            left: 20,
+            right: 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
               child: GestureDetector(
-                onTap: (){},
-                child: Text('All', style: pinkOneStyle.copyWith(
-                  fontSize: 14,
-                  fontWeight: semiBold
-                ),),
+                onTap: () {},
+                child: Text(
+                  'All',
+                  style:
+                      pinkOneStyle.copyWith(fontSize: 14, fontWeight: semiBold),
+                ),
               ),
             ),
             Container(
-              child: Text('On Sale', style: grayOneColorStyle.copyWith(
-                fontSize: 14,
-                fontWeight: semiBold
-              ),),
-            ),
-            Container(
-              child: Text('New arrivals', style: grayOneColorStyle.copyWith(
-                fontSize: 14,
-                fontWeight: semiBold
-              ),),
-            ),
-            Container(
-              child: Text('Package', style: grayOneColorStyle.copyWith(
-                fontSize: 14,
-                fontWeight: semiBold
-              ),),
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                right: 12
+              child: Text(
+                'On Sale',
+                style: grayOneColorStyle.copyWith(
+                    fontSize: 14, fontWeight: semiBold),
               ),
-              child: Text('Healty', style: grayOneColorStyle.copyWith(
-                fontSize: 14,
-                fontWeight: semiBold
-              ),),
+            ),
+            Container(
+              child: Text(
+                'New arrivals',
+                style: grayOneColorStyle.copyWith(
+                    fontSize: 14, fontWeight: semiBold),
+              ),
+            ),
+            Container(
+              child: Text(
+                'Package',
+                style: grayOneColorStyle.copyWith(
+                    fontSize: 14, fontWeight: semiBold),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(right: 12),
+              child: Text(
+                'Healty',
+                style: grayOneColorStyle.copyWith(
+                    fontSize: 14, fontWeight: semiBold),
+              ),
             ),
           ],
         ),
       );
     }
 
-    Widget flashSale(){
+    Widget flashSale() {
       return Container(
         margin: EdgeInsets.only(
-          top: MediaQuery.of(context).size.height * 0.03,
-          left: defaultMargin,
-          right: defaultMargin
-        ),
+            top: MediaQuery.of(context).size.height * 0.03,
+            left: defaultMargin,
+            right: defaultMargin),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
               child: Row(
                 children: [
-                  Text('Flash Sale', style: primaryTextStyle.copyWith(
-                    fontSize: 16,
-                    fontWeight: semiBold
-                  )),
+                  Text('Flash Sale',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 16, fontWeight: semiBold)),
                   SizedBox(
                     width: 12,
                   ),
-                  
+
                   //todo Flash Sale don't Otomatis
                   Container(
                     alignment: Alignment.center,
                     width: MediaQuery.of(context).size.width * 0.1,
                     height: 30,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: pinkOne
+                        borderRadius: BorderRadius.circular(8), color: pinkOne),
+                    child: Text(
+                      '12',
+                      style: backgroundColorStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                      textAlign: TextAlign.center,
                     ),
-                    child: Text('12', style: backgroundColorStyle.copyWith(
-                      fontSize: 14,
-                      fontWeight: semiBold
-                    ), textAlign: TextAlign.center,),
                   ),
                   SizedBox(
                     width: 6,
@@ -212,13 +209,13 @@ class HomePage extends StatelessWidget {
                     width: MediaQuery.of(context).size.width * 0.1,
                     height: 30,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: pinkOne
+                        borderRadius: BorderRadius.circular(8), color: pinkOne),
+                    child: Text(
+                      '04',
+                      style: backgroundColorStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                      textAlign: TextAlign.center,
                     ),
-                    child: Text('04', style: backgroundColorStyle.copyWith(
-                      fontSize: 14,
-                      fontWeight: semiBold
-                    ), textAlign: TextAlign.center,),
                   ),
                   SizedBox(
                     width: 6,
@@ -228,22 +225,66 @@ class HomePage extends StatelessWidget {
                     width: MediaQuery.of(context).size.width * 0.1,
                     height: 30,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: pinkOne
+                        borderRadius: BorderRadius.circular(8), color: pinkOne),
+                    child: Text(
+                      '59',
+                      style: backgroundColorStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                      textAlign: TextAlign.center,
                     ),
-                    child: Text('59', style: backgroundColorStyle.copyWith(
-                      fontSize: 14,
-                      fontWeight: semiBold
-                    ), textAlign: TextAlign.center,),
                   ),
                 ],
               ),
             ),
-            
-            Text('See all', style: secondaryBlackStyle.copyWith(
-              fontSize: 14,
-              fontWeight: semiBold
-            ),)
+            Text(
+              'See all',
+              style: secondaryBlackStyle.copyWith(
+                  fontSize: 14, fontWeight: semiBold),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget productSale() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.03,
+            left: 20,
+            right: 20),
+        width: MediaQuery.of(context).size.width * 0.5,
+        height: MediaQuery.of(context).size.height * 0.3,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 1), // changes position of shadow
+            ),
+          ],
+          borderRadius: BorderRadius.circular(12),
+          color: backgroundColor,
+        ),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.2,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(12),
+                  ),
+                  color: pinkThree),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                      child: Image.asset('assets/images/product_one.png',
+                          width: 160)),
+                ],
+              ),
+            ),
           ],
         ),
       );
@@ -253,11 +294,12 @@ class HomePage extends StatelessWidget {
         body: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        headerNav(), 
-        searchproduct(), 
+        headerNav(),
+        searchproduct(),
         imageSlader(),
         categories(),
-        flashSale()
+        flashSale(),
+        productSale()
       ],
     ));
   }
