4bc956f18a6304b5c39b4e27940363aa5bb9d4ad
Apridev <apriantofajar11@gmail.com>
2022-12-26 21:36:25 +0900

    daily schedule tile

--- lib/widgets/daily_schedule_tile.dart
+++ lib/widgets/daily_schedule_tile.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:heartfilia_app/widgets/schedule_tile.dart';
+import 'package:heartfilia_app/template.dart';
 
 class DailyScheduleTile extends StatelessWidget {
+
+  final String images;
+  final String categories;
+  final String name;
+
+  DailyScheduleTile({
+    required this.images,
+    required this.categories,
+    required this.name
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +24,74 @@ class DailyScheduleTile extends StatelessWidget {
           Column(
             children: [
               Image.asset(
-                'assets/images/image_sun.png',
+                images,
                 width: 40,
               ),
             ],
           ),
-          ScheduleTile(),
+          Expanded(
+      child: Container(
+        margin: EdgeInsets.only(left: defaultMargin),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.1,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: backgroundColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 12, left: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      categories,
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14,
+                          fontWeight: semiBold,
+                          overflow: TextOverflow.ellipsis),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      name,
+                      style: secondaryBlackStyle.copyWith(
+                          fontSize: 12,
+                          fontWeight: medium,
+                          overflow: TextOverflow.ellipsis),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    margin: EdgeInsets.only(right: 12),
+                    child: Image.asset(
+                      'assets/icons/icon_checklist_on.png',
+                      width: 20,
+                    )),
+              ],
+            )
+          ],
+        ),
+      ),
+    )
         ],
       ),
     );
