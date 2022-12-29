6980410db1804ba164e401a9a131419f7c48e0ed
Apridev <apriantofajar11@gmail.com>
2022-12-29 22:14:39 +0900

    calender page

--- lib/components/calender_page.dart
+++ lib/components/calender_page.dart
@@ -14,206 +14,214 @@ class CalenderPage extends StatelessWidget {
           'Daily Schedule',
           style: primaryTextStyle.copyWith(fontSize: 14, fontWeight: semiBold),
         ),
-        actions: [
-          Container(
-            margin: EdgeInsets.only(right: defaultMargin),
-            child: IconButton(
-                onPressed: () {},
-                icon: Image.asset(
-                  'assets/icons/icon_hamburger.png',
-                  width: 24,
-                )),
-          ),
-        ],
         elevation: 0,
+        iconTheme: IconThemeData(color: blackColor),
       ),
-      body: Column(
-        children: [
-          // !!don't fixed automatic
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      endDrawer: Drawer(),
+      body: ListView(
+        children: 
+          [Column(
             children: [
-              IconButton(
-                  onPressed: () {}, icon: Icon(Icons.arrow_back_ios_rounded)),
-              Text(
-                'August',
-                style:
-                    primaryTextStyle.copyWith(fontSize: 14, fontWeight: medium),
+              // !!don't fixed automatic
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.arrow_back_ios_rounded)),
+                  Text(
+                    'August',
+                    style:
+                        primaryTextStyle.copyWith(fontSize: 14, fontWeight: medium),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_forward_ios_rounded)),
+                ],
               ),
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_forward_ios_rounded)),
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.only(
-                top: 14, left: defaultMargin, right: defaultMargin),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Mon',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: semiBold),
+              Container(
+                margin: EdgeInsets.only(
+                    top: 14, left: defaultMargin, right: defaultMargin),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Mon',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                    ),
+                    Text(
+                      'Tue',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                    ),
+                    Text(
+                      'Today',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                    ),
+                    Text(
+                      'Thu',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                    ),
+                    Text(
+                      'Sat',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                    ),
+                    Text(
+                      'Sun',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                    ),
+                    Text(
+                      'Sun',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: semiBold),
+                    )
+                  ],
                 ),
-                Text(
-                  'Tue',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: semiBold),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.02,
+                    left: defaultMargin,
+                    right: defaultMargin),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                        width: 30,
+                        height: 30,
+                        decoration:
+                            BoxDecoration(shape: BoxShape.circle, color: greenOne),
+                        child: Center(
+                          child: Text(
+                            '11',
+                            style: backgroundColorStyle.copyWith(
+                                fontSize: 14, fontWeight: medium),
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
+                    Container(
+                        width: 30,
+                        height: 30,
+                        decoration:
+                            BoxDecoration(shape: BoxShape.circle, color: greenOne),
+                        child: Center(
+                          child: Text(
+                            '12',
+                            style: backgroundColorStyle.copyWith(
+                                fontSize: 14, fontWeight: medium),
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
+                    Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: pinkOne, width: 2)),
+                        child: Center(
+                          child: Text(
+                            '11',
+                            style: pinkOneStyle.copyWith(
+                                fontSize: 14, fontWeight: medium),
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
+                    Text(
+                      '14',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: medium),
+                    ),
+                    Text(
+                      '15',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: medium),
+                    ),
+                    Text(
+                      '16',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: medium),
+                    ),
+                    Text(
+                      '17',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 14, fontWeight: medium),
+                    ),
+                  ],
                 ),
-                Text(
-                  'Today',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: semiBold),
-                ),
-                Text(
-                  'Thu',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: semiBold),
-                ),
-                Text(
-                  'Sat',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: semiBold),
-                ),
-                Text(
-                  'Sun',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: semiBold),
-                ),
-                Text(
-                  'Sun',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: semiBold),
-                )
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.02,
-                left: defaultMargin,
-                right: defaultMargin),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                    width: 30,
-                    height: 30,
-                    decoration:
-                        BoxDecoration(shape: BoxShape.circle, color: greenOne),
-                    child: Center(
-                      child: Text(
-                        '11',
-                        style: backgroundColorStyle.copyWith(
-                            fontSize: 14, fontWeight: medium),
-                        textAlign: TextAlign.center,
-                      ),
-                    )),
-                Container(
-                    width: 30,
-                    height: 30,
-                    decoration:
-                        BoxDecoration(shape: BoxShape.circle, color: greenOne),
-                    child: Center(
-                      child: Text(
-                        '12',
-                        style: backgroundColorStyle.copyWith(
-                            fontSize: 14, fontWeight: medium),
-                        textAlign: TextAlign.center,
-                      ),
-                    )),
-                Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: pinkOne, width: 2)),
-                    child: Center(
-                      child: Text(
-                        '11',
-                        style: pinkOneStyle.copyWith(
-                            fontSize: 14, fontWeight: medium),
-                        textAlign: TextAlign.center,
-                      ),
-                    )),
-                Text(
-                  '14',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: medium),
-                ),
-                Text(
-                  '15',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: medium),
-                ),
-                Text(
-                  '16',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: medium),
-                ),
-                Text(
-                  '17',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 14, fontWeight: medium),
-                ),
-              ],
-            ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.03,
-                      left: defaultMargin,
-                      right: defaultMargin),
-                  child: Column(
-                    children: [
-                      // ??Gesture Detector
-                      DottedBorder(
-                        color: grayTwo,
-                        strokeWidth: 2,
-                        borderType: BorderType.RRect,
-                        radius: Radius.circular(12),
-                        dashPattern: [10, 5],
-                        child: Container(
-                          height: MediaQuery.of(context).size.height * 0.1,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              color: backgroundColor),
-                          child: Container(
-                            margin: EdgeInsets.only(
-                                left: defaultMargin, right: defaultMargin),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Add step / product',
-                                  style: primaryTextStyle.copyWith(
-                                      fontSize: 14, fontWeight: semiBold),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.03,
+                          left: defaultMargin,
+                          right: defaultMargin),
+                      child: Column(
+                        children: [
+                          // ??Gesture Detector
+                          DottedBorder(
+                            color: grayTwo,
+                            strokeWidth: 2,
+                            borderType: BorderType.RRect,
+                            radius: Radius.circular(12),
+                            dashPattern: [10, 5],
+                            child: Container(
+                              height: MediaQuery.of(context).size.height * 0.1,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  color: backgroundColor),
+                              child: Container(
+                                margin: EdgeInsets.only(
+                                    left: defaultMargin, right: defaultMargin),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Add step / product',
+                                      style: primaryTextStyle.copyWith(
+                                          fontSize: 14, fontWeight: semiBold),
+                                    ),
+                                    Image.asset(
+                                      'assets/icons/icon_create_calender.png',
+                                      width: 27,
+                                    )
+                                  ],
                                 ),
-                                Image.asset(
-                                  'assets/icons/icon_create_calender.png',
-                                  width: 27,
-                                )
-                              ],
+                              ),
                             ),
                           ),
-                        ),
+                          // ?? Not Fixed
+                          DailyScheduleTile(
+                            images: 'assets/images/image_moon.png',
+                            categories: 'Sun Screen',
+                            name: 'Wardah UV Shield Essential Sunscreen...',
+                            checkMode: 'assets/icons/icon_checklist_on.png',
+                          ),
+                          DailyScheduleTile(
+                            images: 'assets/images/image_sun.png',
+                            categories: 'Toner',
+                            name: "Kiehl's Calendula Herbal Extract Alcohol...",
+                            checkMode: "assets/icons/icon_checklist_on.png",
+                          ),
+                          DailyScheduleTile(
+                            images: 'assets/images/image_moon.png',
+                            categories: 'Clenser',
+                            name: "Garnier Micellar Cleansing Water Pink ",
+                            checkMode: "assets/icons/icon_checklist_off.png",
+                          ),
+                        ],
                       ),
-                      // ?? Not Fixed
-                      DailyScheduleTile(
-                        images: 'assets/images/image_sun.png',
-                        categories: 'Sun Screen',
-                        name: 'Wardah UV Shield Essential Sunscreen...',
-                      )
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
