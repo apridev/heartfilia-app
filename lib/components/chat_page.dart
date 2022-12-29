a626e5e083f3903d37852538e615726ffb62ee90
Apridev <apriantofajar11@gmail.com>
2022-12-29 22:14:17 +0900

    chat page drawer

--- lib/components/chat_page.dart
+++ lib/components/chat_page.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
 
 class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text('Chat Page'))
-        ],
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        title: Text(
+          'Your Chat',
+          style: primaryTextStyle.copyWith(fontSize: 14, fontWeight: semiBold),
+        ),
+        elevation: 0,
+        iconTheme: IconThemeData(color: blackColor),
       ),
+      endDrawer: Drawer(),
     );
   }
 }
