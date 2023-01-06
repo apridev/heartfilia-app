4f580e3863833910c1aab0b15e48720f064b7ba9
Apridev <apriantofajar11@gmail.com>
2023-01-06 22:40:24 +0900

    detail chat page

---
+++ lib/components/detail_chat_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
+
+class DetailChatPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Datail Chat Page'),
+          )
+        ],
+      ),
+    );
+  }
+}
