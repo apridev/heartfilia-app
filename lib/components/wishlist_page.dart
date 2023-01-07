98dcf17255748f77b8d624540163abfda1ec2a5f
Apridev <apriantofajar11@gmail.com>
2023-01-07 13:08:33 +0900

    wishlist page revision

--- lib/components/wishlist_page.dart
+++ lib/components/wishlist_page.dart
@@ -26,7 +26,7 @@ class WishlistPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    height: 210,
+                    height: MediaQuery.of(context).size.height * 0.27,
                     width: double.infinity,
                     decoration: BoxDecoration(color: pinkOne),
                     child: Column(
