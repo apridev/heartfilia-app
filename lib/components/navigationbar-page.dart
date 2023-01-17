1dfd10ed0b1364e60d58501ed037abf6d43c499b
Apridev <apriantofajar11@gmail.com>
2023-01-17 13:34:12 +0900

    notificationsbar page

--- lib/components/navigationbar-page.dart
+++ lib/components/navigationbar-page.dart
@@ -4,33 +4,30 @@ import 'package:heartfilia_app/components/chat_page.dart';
 import 'package:heartfilia_app/components/home_page.dart';
 import 'package:heartfilia_app/components/wishlist_page.dart';
 import 'package:heartfilia_app/home/login_page.dart';
+import 'package:heartfilia_app/home/profile_page.dart';
 import 'package:heartfilia_app/template.dart';
 
 class NavigationbarPage extends StatefulWidget {
-
   @override
   State<NavigationbarPage> createState() => _NavigationbarPageState();
 }
 
 class _NavigationbarPageState extends State<NavigationbarPage> {
-
-    int currentIndex = 0;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     Widget buttomNavbar() {
       return Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: secondaryBlackColor,
-            )
-          ]
-        ),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: secondaryBlackColor,
+          )
+        ]),
         child: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           backgroundColor: backgroundColor,
-          onTap: (value){
+          onTap: (value) {
             setState(() {
               currentIndex = value;
               // print(value);
@@ -39,57 +36,72 @@ class _NavigationbarPageState extends State<NavigationbarPage> {
           items: [
             BottomNavigationBarItem(
                 icon: Container(
-                  padding: EdgeInsets.only(
-                    top: 12
-                  ),
-                    child: currentIndex == 0 ? Image.asset(
-                  'assets/icons/icon_home_active.png',
-                  width: MediaQuery.of(context).size.width * 0.07,
-                ) : Image.asset(
-                  'assets/icons/icon_home.png',
-                  width: MediaQuery.of(context).size.width * 0.07,
-                ) ),
+                    padding: EdgeInsets.only(top: 12),
+                    child: currentIndex == 0
+                        ? Image.asset(
+                            'assets/icons/icon_home_active.png',
+                            width: MediaQuery.of(context).size.width * 0.07,
+                          )
+                        : Image.asset(
+                            'assets/icons/icon_home.png',
+                            width: MediaQuery.of(context).size.width * 0.07,
+                          )),
                 label: ''),
             BottomNavigationBarItem(
                 icon: Container(
-                  padding: EdgeInsets.only(
-                    top: 12,
-                  ),
-                  child: currentIndex == 1 ? Image.asset(
-                    'assets/icons/icon_calender_active.png',
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  ): Image.asset(
-                    'assets/icons/icon_calender.png',
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  )
+                    padding: EdgeInsets.only(
+                      top: 12,
+                    ),
+                    child: currentIndex == 1
+                        ? Image.asset(
+                            'assets/icons/icon_calender_active.png',
+                            width: MediaQuery.of(context).size.width * 0.07,
+                          )
+                        : Image.asset(
+                            'assets/icons/icon_calender.png',
+                            width: MediaQuery.of(context).size.width * 0.07,
+                          )),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding: EdgeInsets.only(top: 12),
+                  child: currentIndex == 2
+                      ? Image.asset(
+                          'assets/icons/icon_like_active.png',
+                          width: MediaQuery.of(context).size.width * 0.07,
+                        )
+                      : Image.asset(
+                          'assets/icons/icon_like.png',
+                          width: MediaQuery.of(context).size.width * 0.07,
+                        ),
                 ),
                 label: ''),
             BottomNavigationBarItem(
                 icon: Container(
-                  padding: EdgeInsets.only(
-                    top: 12
-                  ),
-                  child: currentIndex == 2 ? Image.asset(
-                    'assets/icons/icon_chat_active.png',
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  ) : Image.asset(
-                    'assets/icons/icon_chat.png',
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  ),
+                  padding: EdgeInsets.only(top: 12),
+                  child: currentIndex == 3
+                      ? Image.asset(
+                          'assets/icons/icon_chat_active.png',
+                          width: MediaQuery.of(context).size.width * 0.07,
+                        )
+                      : Image.asset(
+                          'assets/icons/icon_chat.png',
+                          width: MediaQuery.of(context).size.width * 0.07,
+                        ),
                 ),
                 label: ''),
             BottomNavigationBarItem(
                 icon: Container(
-                  padding: EdgeInsets.only(
-                    top: 12
-                  ),
-                  child: currentIndex == 3 ? Image.asset(
-                    'assets/icons/icon_like_active.png',
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  ) : Image.asset(
-                    'assets/icons/icon_like.png',
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  ),
+                  padding: EdgeInsets.only(top: 12),
+                  child: currentIndex == 4
+                      ? Image.asset(
+                          'assets/icons/icon_profile_active.png',
+                          width: MediaQuery.of(context).size.width * 0.07,
+                        )
+                      : Image.asset(
+                          'assets/icons/icon_profile.png',
+                          width: MediaQuery.of(context).size.width * 0.07,
+                        ),
                 ),
                 label: ''),
           ],
@@ -97,7 +109,7 @@ class _NavigationbarPageState extends State<NavigationbarPage> {
       );
     }
 
-    Widget bodyHome(){
+    Widget bodyHome() {
       switch (currentIndex) {
         case 0:
           return HomePage();
@@ -106,20 +118,23 @@ class _NavigationbarPageState extends State<NavigationbarPage> {
           return CalenderPage();
           break;
         case 2:
-          return ChatPage();
-          break;
-        case 3:
           return WishlistPage();
           break;
+        case 3:
+          return ChatPage();
+          break;
+        case 4:
+          return ProfilePage();
+          break;
         default:
-        return HomePage();
+          return HomePage();
       }
     }
 
     return Scaffold(
-        backgroundColor: backgroundColor, 
-        bottomNavigationBar: buttomNavbar(),
-        body: bodyHome() ,
-        );
+      backgroundColor: backgroundColor,
+      bottomNavigationBar: buttomNavbar(),
+      body: bodyHome(),
+    );
   }
 }
