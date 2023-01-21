f8668b61a153c65281ec154f33ed7ca30a03388b
Apridev <apriantofajar11@gmail.com>
2023-01-21 13:41:46 +0900

    list icon help center tile

---
+++ lib/widgets/list-icon-help-center-tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/widgets/list_profile_tile.dart';
+
+class ListIconHelpCenterTile extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListProfileTile(
+          icons: 'assets/icons/icon_profile_complaint_order.png', 
+          nameIcons: 'Complaint order'
+        ),
+        ListProfileTile(
+          icons: 'assets/icons/icon_profile_help_center.png', 
+          nameIcons: 'Heartfillia help center'
+        ),
+        ListProfileTile(
+          icons: 'assets/icons/icon_profile_logout_account.png', 
+          nameIcons: 'Logout account'
+        ),
+      ],
+    );
+  }
+}
