f4bdfb3cfe764dd641a322f8b260180100ac8b18
Apridev <apriantofajar11@gmail.com>
2023-01-24 13:30:12 +0900

    add schedule page

---
+++ lib/components/add_schedule.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
+
+class AddSchedule extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('Add Schedule')
+          )
+        ],
+      ),
+    );
+  }
+}
