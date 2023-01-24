72b5da0ce745f9e3bf99a759b991b6fe392cfaa3
Apridev <apriantofajar11@gmail.com>
2023-01-24 13:35:21 +0900

    edit profile page

---
+++ lib/components/edit-profile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
+
+class EditProfile extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Edit Profile'),
+          )
+        ],
+      ),
+    );
+  }
+}
