071911a5c0c44b54eeca21968ff2db5d29d69184
Apridev <apriantofajar11@gmail.com>
2023-01-28 21:38:57 +0900

    delete feature search

--- lib/components/chat_page.dart
+++ lib/components/chat_page.dart
@@ -17,12 +17,9 @@ class ChatPage extends StatelessWidget {
           children: [
             Text(
               'Your Chat',
-              style: primaryTextStyle.copyWith(fontSize: 14, fontWeight: semiBold),
+              style:
+                  primaryTextStyle.copyWith(fontSize: 14, fontWeight: semiBold),
             ),
-            IconButton(
-              onPressed: (){}, 
-              icon: Icon(Icons.filter_list)
-            )
           ],
         ),
         elevation: 0,
@@ -35,60 +32,13 @@ class ChatPage extends StatelessWidget {
           color: pinkOne,
           onRefresh: _refresh,
           child: GestureDetector(
-            onTap: (){
+            onTap: () {
               FocusManager.instance.primaryFocus?.unfocus();
             },
             child: ListView(
               children: [
                 Column(
                   children: [
-                    Container(
-                  margin: EdgeInsets.only(left: 20, right: 20),
-                  child: Container(
-            margin: EdgeInsets.only(
-              bottom: 10
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                      child: TextField(
-                    textAlign: TextAlign.start,
-                    textAlignVertical: TextAlignVertical.center,
-                    textInputAction: TextInputAction.search,
-                    cursorColor: pinkOne,
-                    style: TextStyle(color: blackColor),
-                    // obscureText: true,
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(12),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
-                            borderSide: BorderSide(color: pinkOne)),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
-                            borderSide: BorderSide(color: grayTwo)),
-                        // prefixIcon: Icon(Icons.person),
-                        prefixIcon: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Icon(Icons.search)],
-                        ),
-                        hintText: "Searching chat...",
-                        hintStyle: secondaryBlackStyle.copyWith(fontSize: 14)),
-                  )),
-                ),
-              ],
-            ),
-                  ),
-                ),
-                    ChatTile(
-                      images: 'assets/images/image_chat.png',
-                      nameUser: 'Heartfilia Center',
-                      description:
-                          'Good night, This item is on item is on This item is on item is on',
-                      time: 'Now',
-                    ),
                     ChatTile(
                       images: 'assets/images/image_chat.png',
                       nameUser: 'Heartfilia Center',
@@ -107,8 +57,6 @@ class ChatPage extends StatelessWidget {
   }
 
   Future<void> _refresh() {
-    return Future.delayed(
-      Duration(seconds: 2)
-    );
+    return Future.delayed(Duration(seconds: 2));
   }
 }
