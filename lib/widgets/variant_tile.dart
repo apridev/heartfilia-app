e938a59524b22895dd33ba2f6891b0e0706f4a65
Apridev <apriantofajar11@gmail.com>
2023-02-03 21:38:34 +0900

    variant product

---
+++ lib/widgets/variant_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
+
+class VariantTile extends StatelessWidget {
+
+  final String name;
+  final TextStyle color;
+  final Color border;
+
+  VariantTile({
+    required this.name, required this.color, required this.border
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        right: 10
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      height: 40,
+      decoration: BoxDecoration(
+          border: Border.all(color: border),
+          borderRadius: BorderRadius.circular(12)),
+      child: TextButton(
+          onPressed: () {},
+          child: Text(
+            name,
+            style: color.copyWith(fontSize: 12, fontWeight: medium),
+          )),
+    );
+  }
+}
