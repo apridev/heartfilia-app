4c2a556c3468815f33309c438228132259db6d8f
Apridev <apriantofajar11@gmail.com>
2023-02-06 21:20:47 +0900

    cart page update

--- lib/home/cart-page.dart
+++ lib/home/cart-page.dart
@@ -8,14 +8,41 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: ListView(
-        children: 
-          [
-            Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(child: Text('Cart Page'))
-            ],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: backgroundColor,
+            titleSpacing: 0,
+            elevation: 0,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Image.asset(
+                        'assets/icons/icons_arrow_left_cart.png',
+                        width: 24,
+                      )),
+                ),
+                Text('Cart', style: primaryTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold
+                ),),
+                Container(
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Image.asset(
+                        'assets/icons/icon_options_cart.png',
+                        width: 24,
+                      )),
+                ),
+                
+              ],
+            ),
           ),
         ],
       ),
