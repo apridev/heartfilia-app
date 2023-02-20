4c645713bd83f2974b784eb24531df9d75dce5fc
Apridev <apriantofajar11@gmail.com>
2023-02-20 12:39:52 +0900

    data schedule tile

---
+++ lib/widgets/data_schedule_tile.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
+
+class DataScheduleTile extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: defaultMargin
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '10 am',
+            style: primaryTextStyle.copyWith(fontSize: 13, fontWeight: semiBold),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: MediaQuery.of(context).size.height * 0.12,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: backgroundColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: Offset(0, 1), // changes position of shadow
+                  ),
+                ]),
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sun Screen',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 14, fontWeight: semiBold),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Wardah UV Shield Essential Sunscreen... ',
+                    style: secondaryBlackStyle.copyWith(
+                        fontSize: 13,
+                        fontWeight: medium,
+                        overflow: TextOverflow.ellipsis),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    '10 am - 5 pm',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 13, fontWeight: semiBold),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
