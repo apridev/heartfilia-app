f7a8e4daf4ef1c41968519ae1196f15203c2f6ee
Apridev <apriantofajar11@gmail.com>
2023-02-21 21:39:32 +0900

    create calender

---
+++ lib/components/create_calendar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
+
+class CreateCalendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Create Calendar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
