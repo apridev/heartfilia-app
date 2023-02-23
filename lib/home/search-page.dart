96e05218a88cc93444a3fa5110e80552afa641c6
Apridev <apriantofajar11@gmail.com>
2023-02-23 14:53:08 +0900

    search page update

--- lib/home/search-page.dart
+++ lib/home/search-page.dart
@@ -6,30 +6,39 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: ListView(
-        children: 
-            [Container(
-              margin: EdgeInsets.only(
-                right: 10,
-                left: 10
-              ),
-              child: Row(
+      body: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: backgroundColor,
+              titleSpacing: 0,
+              elevation: 0,
+              title: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  IconButton(
-                    onPressed: (){
-                      Navigator.pop(context);
-                    }, 
-                    icon: Icon(Icons.arrow_back_rounded, color: blackColor,)),
-                  Image.asset('assets/images/heartfilia_home.png',
-                      width: MediaQuery.of(context).size.width * 0.2),
+                  Container(
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Image.asset(
+                          'assets/icons/icons_arrow_left_cart.png',
+                          width: 24,
+                        )),
+                  ),
+                  Image.asset(
+                    'assets/images/heartfilia_home.png',
+                    width: MediaQuery.of(context).size.width * 0.2,
+                  ),
                   Container(
                     child: Row(
                       children: [
                         IconButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/cart-page');
-                            },
+                            onPressed: () {},
                             icon: Image.asset(
                               'assets/icons/icon_cart.png',
                               width: 24,
@@ -46,7 +55,63 @@ class SearchPage extends StatelessWidget {
                 ],
               ),
             ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20, right: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                              child: TextField(
+                            textAlign: TextAlign.start,
+                            textAlignVertical: TextAlignVertical.center,
+                            textInputAction: TextInputAction.search,
+                            cursorColor: secondaryBlackColor,
+                            autofocus: true,
+                            style: TextStyle(color: blackColor),
+                            // obscureText: true,
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(12),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(18)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(18),
+                                    borderSide: BorderSide(color: grayTwo)),
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(18),
+                                    borderSide: BorderSide(color: grayTwo)),
+                                // prefixIcon: Icon(Icons.person),
+                                prefixIcon: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      'assets/icons/icons_search.png',
+                                      width: 18,
+                                    )
+                                  ],
+                                ),
+                                hintText: "I’m looking for...",
+                                hintStyle:
+                                    secondaryBlackStyle.copyWith(fontSize: 14)),
+                          )),
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Image.asset(
+                          'assets/icons/icon_hamburger_filter.png',
+                          width: MediaQuery.of(context).size.width * 0.12,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
+        ),
       ),
     );
   }
