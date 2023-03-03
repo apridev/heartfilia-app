08e6f469275afdbb9559c3e85e26c866d9d36373
Apridev <apriantofajar11@gmail.com>
2023-03-03 15:09:15 +0900

    checkout page

---
+++ lib/components/checkout_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:heartfilia_app/template.dart';
+
+class CheckOutPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Checkout Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
